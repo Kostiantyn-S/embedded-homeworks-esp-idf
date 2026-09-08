d631559ca13ecc0c04331a9daeaff538f38b4815
Kostiantyn2023 <kostiantyn.starchyk@gamingcorps.com>
2026-09-08 21:56:56 +0300

    homework_3_3 implementation

--- src/config.h
+++ src/config.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#define POT_ADC_CHANNEL       ADC_CHANNEL_3
-#define ADC_ATTEN             ADC_ATTEN_DB_12
-#define ADC_BITWIDTH_CONFIG   ADC_BITWIDTH_12
-#define READ_INTERVAL_US      (100ULL * 1000ULL)
+#include <driver/gpio.h>
 
-#define SMA_WINDOW_SIZE       10
-
-#define THRESHOLD_DARK        1800  
-#define THRESHOLD_LIGHT       2200
+constexpr static gpio_num_t LED_GPIO = GPIO_NUM_18;
+constexpr static gpio_num_t POT_GPIO = GPIO_NUM_6;
+constexpr static gpio_num_t BASE_TRANSISTOR_GPIO = GPIO_NUM_4;
+constexpr static int READ_INTERVAL_US = (100ULL * 1000ULL);
+constexpr static adc_channel_t POT_ADC_CHANNEL = ADC_CHANNEL_5;
+constexpr static adc_atten_t ADC_ATTEN = ADC_ATTEN_DB_12;
+constexpr static adc_bitwidth_t ADC_BITWIDTH_CONFIG = ADC_BITWIDTH_12;
--- src/main.cpp
+++ src/main.cpp
@@ -5,62 +5,44 @@
 #include <freertos/task.h>
 #include <cmath>
 #include <driver/gpio.h>
+#include <driver/ledc.h>
 
 #include "config.h"
 
 static adc_oneshot_unit_handle_t adc_handle;
-static int sma_buffer[SMA_WINDOW_SIZE];
-static int sma_index = 0;
-static int sma_sum = 0;
-static bool sma_buffer_filled = false;
-static int counter = 0;
-static bool led_is_on = false;
-static const gpio_num_t LED_GPIO = GPIO_NUM_18;
-
-void update_led_state(int sma_value) {
-    if (!led_is_on && sma_value < THRESHOLD_DARK) {
-        led_is_on = true;
-        gpio_set_level(LED_GPIO, 1);
-    } else if (led_is_on && sma_value > THRESHOLD_LIGHT) {
-        led_is_on = false;
-        gpio_set_level(LED_GPIO, 0);
-    }
-}
-
-static void sma_add_value(int raw) {
-    if (!sma_buffer_filled) {
-        sma_buffer[counter] = raw;
-        counter++;
-        sma_sum += raw;
-
-        if (counter >= SMA_WINDOW_SIZE) {
-            sma_buffer_filled = true;
-        }
-    } else {
-        sma_sum -= sma_buffer[sma_index];
-        sma_buffer[sma_index] = raw;
-        sma_sum += raw;
-        sma_index++;
-        if (sma_index % SMA_WINDOW_SIZE == 0) {
-            sma_index = 0;
-        }
-    }
-}
 
 static void adc_read_callback(void* arg) {
     int raw = 0;
     adc_oneshot_read(adc_handle, POT_ADC_CHANNEL, &raw);
 
-    sma_add_value(raw);
+    int duty = raw * 1023 / 4095;
 
-    int divisor = sma_buffer_filled ? SMA_WINDOW_SIZE : counter;
-    if (divisor > 0) {
-        printf("%4d\n", sma_sum / divisor);
-        update_led_state(sma_sum / divisor);
-    }
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty);
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, duty);
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1);
 }
 
 extern "C" void app_main(void) {
+    /* LED */
+    ledc_timer_config_t led_timer_conf = {};
+    led_timer_conf.speed_mode = LEDC_LOW_SPEED_MODE;
+    led_timer_conf.timer_num = LEDC_TIMER_0;
+    led_timer_conf.duty_resolution = LEDC_TIMER_10_BIT;
+    led_timer_conf.freq_hz = 5000;
+    led_timer_conf.clk_cfg = LEDC_AUTO_CLK;
+    ledc_timer_config(&led_timer_conf);
+
+    ledc_channel_config_t led_channel_conf = {};
+    led_channel_conf.gpio_num = LED_GPIO;
+    led_channel_conf.speed_mode = LEDC_LOW_SPEED_MODE;
+    led_channel_conf.channel = LEDC_CHANNEL_0;
+    led_channel_conf.timer_sel = LEDC_TIMER_0;
+    led_channel_conf.duty = 0;
+    ledc_channel_config(&led_channel_conf);
+
+    /* POT */
     adc_oneshot_unit_init_cfg_t init_config = {};
     init_config.unit_id = ADC_UNIT_1;
     adc_oneshot_new_unit(&init_config, &adc_handle);
@@ -70,17 +52,30 @@ extern "C" void app_main(void) {
     chan_config.atten = ADC_ATTEN;
     adc_oneshot_config_channel(adc_handle, POT_ADC_CHANNEL, &chan_config);
 
-    gpio_config_t io_conf = {};
-    io_conf.pin_bit_mask = (1ULL << LED_GPIO);
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    gpio_config(&io_conf);
-
+    /* TIMER */
     esp_timer_create_args_t read_timer_args = {};
     esp_timer_handle_t read_timer;
     read_timer_args.callback = &adc_read_callback;
     read_timer_args.name = "adc_read_timer";
     esp_timer_create(&read_timer_args, &read_timer);
     esp_timer_start_periodic(read_timer, READ_INTERVAL_US);
+
+    /* MOTOR */
+    ledc_timer_config_t motor_timer_conf = {};
+    motor_timer_conf.speed_mode = LEDC_LOW_SPEED_MODE;
+    motor_timer_conf.timer_num = LEDC_TIMER_1;
+    motor_timer_conf.duty_resolution = LEDC_TIMER_10_BIT;
+    motor_timer_conf.freq_hz = 1000;
+    motor_timer_conf.clk_cfg = LEDC_AUTO_CLK;
+    ledc_timer_config(&motor_timer_conf);
+
+    ledc_channel_config_t motor_channel_conf = {};
+    motor_channel_conf.gpio_num = BASE_TRANSISTOR_GPIO;
+    motor_channel_conf.speed_mode = LEDC_LOW_SPEED_MODE;
+    motor_channel_conf.channel = LEDC_CHANNEL_1;
+    motor_channel_conf.timer_sel = LEDC_TIMER_1;
+    motor_channel_conf.duty = 0;
+    ledc_channel_config(&motor_channel_conf);
 
     while (true) {
         vTaskDelay(pdMS_TO_TICKS(1000));
